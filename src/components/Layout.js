01ee4ce874d31c13e8798944d7d2e48c6abe5a39
daiqian <cuit980@gmail.com>
2024-06-19 20:55:40 -0500

    color readjustment

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,25 +2,24 @@ import React from 'react';
 
 const Layout = () => {
   return (
-    <div className="container mx-auto my-5 p-3 rounded-lg border-2" style={{ borderColor: '#abd1e1', maxWidth: '1200px', backgroundColor: '#f3f4f6' }}>
+    <div className="container mx-auto my-5 p-3 rounded-lg shadow-lg" style={{ maxWidth: '1200px', backgroundColor: '#f3f4f6' }}>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-5/12 mb-3">
-          {/* FOCAL IMAGE */}
           <div
             style={{
-              backgroundImage: "url('https://f2.toyhou.se/file/f2-toyhou-se/images/75108968_XHJnFGLblspesP0.png')",
+              backgroundImage: "url('/images/profile/photo1.png')",
               backgroundSize: 'cover',
               backgroundPosition: 'center',
               backgroundRepeat: 'no-repeat',
               borderRadius: '16px',
               minHeight: '450px',
+              boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', 
             }}
             className="rounded-lg h-full"
           ></div>
         </div>
         <div className="w-full lg:w-5/12 px-3">
-          {/* TITLE BLOCK */}
-          <div className="relative py-3 mb-3 rounded-lg" style={{ backgroundColor: ' #c5e6f4' }}>
+          <div className="relative py-3 mb-3 rounded-lg" style={{ backgroundColor: ' #d7d7d7', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
             <div className="text-white text-center py-3">
               <h1 className="text-3xl font-bold uppercase mb-1" style={{ letterSpacing: '2.5px' }}>
                 Kagu ✿ 白板
@@ -30,9 +29,7 @@ const Layout = () => {
               </p>
             </div>
           </div>
-          {/* ABOUT BLOCK */}
-          <div className="p-3 bg-gray-100 mb-3 rounded-lg overflow-auto" style={{ height: '270px', lineHeight: '1.4' }}>
-            <p>Write whatever you want here! this box will scroll when filled.</p>
+          <div className="p-3 bg-gray-100 mb-3 rounded-lg overflow-auto" style={{ height: '270px', lineHeight: '1.4', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis eget risus eu fermentum. Sed leo mi, luctus vitae
               porttitor et, viverra ut lacus. Mauris bibendum, risus in efficitur dignissim, sapien enim pulvinar dui, a gravida elit
@@ -45,7 +42,6 @@ const Layout = () => {
         </div>
         <div className="w-full lg:w-2/12">
           <div className="h-full flex flex-col justify-between">
-            {/* DECO IMAGES */}
             <div
               style={{
                 backgroundImage: "url('https://f2.toyhou.se/file/f2-toyhou-se/images/17777845_Ja7J7dn53mMdWIX.png')",
@@ -53,6 +49,7 @@ const Layout = () => {
                 backgroundPosition: 'center',
                 minHeight: '150px',
                 borderRadius: '16px',
+                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
               }}
               className="mb-3"
             ></div>
@@ -63,6 +60,7 @@ const Layout = () => {
                 backgroundPosition: 'center',
                 minHeight: '150px',
                 borderRadius: '16px',
+                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
               }}
               className="mb-3"
             ></div>
@@ -73,6 +71,7 @@ const Layout = () => {
                 backgroundPosition: 'center',
                 minHeight: '150px',
                 borderRadius: '16px',
+                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
               }}
               className="mb-3"
             ></div>
