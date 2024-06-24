aa20d0761765d4ce0f772c4a008e50252fa1c1aa
daiqian <cuit980@gmail.com>
2024-06-23 23:31:50 -0500

    more pictures

--- src/pages/illustrations.js
+++ src/pages/illustrations.js
@@ -2,16 +2,50 @@ import React, { useState } from 'react';
 import Head from 'next/head';
 import Background from '@/components/Background';
 
-const images = [
-  '/images/illustrations/1.png',
-  '/images/illustrations/2.png',
-  '/images/illustrations/3.png',
-  '/images/illustrations/4.png',
-  // Add more images here if needed to fill the 4x4 grid
+const sections = [
+    {
+        title: 'Regular Style',
+        images: [
+            '/images/illustrations/13.png',
+            '/images/illustrations/14.png',
+            '/images/illustrations/15.jpg',
+            '/images/illustrations/16.png',
+        ]
+    },
+  {
+    title: 'Chibi',
+    images: [
+      '/images/illustrations/1.png',
+      '/images/illustrations/2.png',
+      '/images/illustrations/3.png',
+      '/images/illustrations/4.png',
+      '/images/illustrations/5.png',
+      '/images/illustrations/6.png',
+      '/images/illustrations/7.png',
+      '/images/illustrations/8.png',
+    ]
+  },
+  {
+    title: 'Pixel Art',
+    images: [
+      '/images/illustrations/9.gif',
+      '/images/illustrations/10.gif',
+      '/images/illustrations/11.gif',
+      '/images/illustrations/12.gif',
+    ]
+  },
 ];
 
 const Illustrations = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (src) => {
+    setSelectedImage(src);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
 
   return (
     <>
@@ -21,31 +55,36 @@ const Illustrations = () => {
       </Head>
 
       <Background>
-        <div className="h-screen flex flex-col justify-start">
-          <div className="container mx-auto p-4 flex-grow">
-            <h1 className="text-6xl font-bold text-beige mb-4 text-center mt-8">Chibis</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-              {images.map((src, index) => (
-                <div
-                  key={index}
-                  className="overflow-hidden rounded-lg shadow-lg cursor-pointer w-full h-90" // Set fixed width and height
-                  onClick={() => setSelectedImage(src)}
-                >
-                  <img src={src} alt={`Illustration ${index + 1}`} className="w-full h-full object-cover" />
+        <div className="min-h-screen flex flex-col">
+          <div className="flex-grow">
+            {sections.map((section, sectionIndex) => (
+              <div key={sectionIndex} className="container mx-auto p-4">
+                <h1 className="text-6xl font-bold text-beige mb-8 text-center mt-8">{section.title}</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                  {section.images.map((src, index) => (
+                    <div
+                      key={index}
+                      className="overflow-hidden rounded-lg shadow-lg cursor-pointer w-full h-90"
+                      onClick={() => handleImageClick(src)}
+                    >
+                      <img src={src} alt={`${section.title} ${index + 1}`} className="w-full h-full object-cover" />
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
+
+          {selectedImage && (
+            <div
+              className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+              onClick={handleCloseModal}
+            >
+              <img src={selectedImage} alt="Selected" className="max-w-full max-h-full" />
+            </div>
+          )}
         </div>
 
-        {selectedImage && (
-          <div
-            className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-            onClick={() => setSelectedImage(null)}
-          >
-            <img src={selectedImage} alt="Selected" className="max-w-full max-h-full" />
-          </div>
-        )}
       </Background>
     </>
   );
