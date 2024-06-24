05c7f4096a1acac21aa211fd89d337d218514569
daiqian <cuit980@gmail.com>
2024-06-24 18:56:19 -0500

    sidebar adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,13 +28,13 @@ const NavBar = () => {
 
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between relative' style={{ backgroundColor: '#efcdc2' }}>
-      <button className="flex-col justify-center items-center lg:hidden absolute left-8" onClick={handleClick}>
+      <button className="flex-col justify-center items-center lg:hidden absolute top-1/2 -translate-y-1/2 left-8 z-50" onClick={handleClick}>
         <span className={`dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
         <span className={`dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
         <span className={`dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
       </button>
 
-      <div className={`w-full lg:flex lg:items-center lg:justify-between ${isOpen ? 'flex' : 'hidden'} flex-col lg:flex-row`}>
+      <div className={`w-full lg:flex lg:items-center lg:justify-between ${isOpen ? 'hidden lg:flex' : 'hidden'}`}>
         <nav className="flex flex-col lg:flex-row lg:items-center">
           <CustomLink href="/" title="&#10047; Home" className='lg:mr-4' />
           <CustomLink href="/about" title="&#10047; About" className='lg:mx-4' />
@@ -43,26 +43,51 @@ const NavBar = () => {
         </nav>
 
         <nav className="flex items-center justify-center mt-4 lg:mt-0">
-          <motion.a href="https://twitter.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mr-3">
+          <motion.a href="https://twitter.com" target="_blank" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mr-3">
             <TwitterIcon style={{ fill: '#FFFFFF' }} />
           </motion.a>
-          <motion.a href="https://github.com" target={"_blank"} className="w-6 mx-3" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
+          <motion.a href="https://github.com" target="_blank" className="w-6 mx-3" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
             <GithubIcon style={{ fill: '#FFFFFF' }} />
           </motion.a>
-          <motion.a href="https://linkedin.com" target={"_blank"} className="w-6 ml-3" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
+          <motion.a href="https://linkedin.com" target="_blank" className="w-6 ml-3" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
             <LinkedInIcon style={{ fill: '#FFFFFF' }} />
           </motion.a>
         </nav>
       </div>
-
-
-      
 
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo style={{ color: '#FFFFFF' }} />
       </div>
 
       <div className="w-full h-[2px] absolute bottom-0 left-0" style={{ backgroundColor: '#fff' }}></div>
+
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden" onClick={handleClick}></div>
+      )}
+      <div className={`fixed top-0 left-0 h-full bg-[#efcdc2] z-50 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} lg:hidden`} style={{ width: '55%' }}>
+        <button className="absolute top-5 left-10 text-2xl text-white z-50" onClick={handleClick}>
+          X
+        </button>
+
+        <nav className="flex flex-col items-center mt-20 space-y-6">
+          <CustomLink href="/" title="&#10047; Home" />
+          <CustomLink href="/about" title="&#10047; About" />
+          <CustomLink href="/projects" title="&#10047; Projects" />
+          <CustomLink href="/illustrations" title="&#10047; Illustrations" />
+        </nav>
+
+        <nav className="flex items-center justify-center mt-8">
+          <motion.a href="https://twitter.com" target="_blank" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3">
+            <TwitterIcon style={{ fill: '#FFFFFF' }} />
+          </motion.a>
+          <motion.a href="https://github.com" target="_blank" className="w-6 mx-3" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
+            <GithubIcon style={{ fill: '#FFFFFF' }} />
+          </motion.a>
+          <motion.a href="https://linkedin.com" target="_blank" className="w-6 mx-3" whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
+            <LinkedInIcon style={{ fill: '#FFFFFF' }} />
+          </motion.a>
+        </nav>
+      </div>
     </header>
   );
 };
