0fedb3056416899dad97716e5659c8d932260e60
daiqian <cuit980@gmail.com>
2024-06-29 17:48:36 -0500

    t

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,8 @@ export default function Home() {
         <main className="flex items-center justify-center text-dark w-full min-h-screen"
               style={{ 
                 backgroundColor: '#f4f5f6',
-                backgroundImage: "url('/13yao/images/background/square.png')",
+                backgroundImage: "url('images/background/square.png')",
+                backgroundImage: "url('13yao/images/background/square.png')",
                 backgroundRepeat: 'repeat',
                 backgroundSize: '400px 400px'
               }}>
