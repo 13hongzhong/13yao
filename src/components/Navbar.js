25c00319186d05edfd3e618985c3f3096c62db9b
daiqian <cuit980@gmail.com>
2024-06-29 01:45:55 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,9 +49,9 @@ const NavBar = () => {
       <div className={`w-full lg:flex lg:items-center lg:justify-between ${isOpen ? 'hidden lg:flex' : 'hidden'}`}>
         <nav className="flex flex-col lg:flex-row lg:items-center">
           <CustomLink href="/13yao" title="&#10047; Home" className='lg:mr-4' onClick={handleClick} />
-          <CustomLink href="/skills" title="&#10047; Skills" className='lg:mx-4' onClick={handleClick} />
-          <CustomLink href="/projects" title="&#10047; Projects" className='lg:mx-4' onClick={handleClick} />
-          <CustomLink href="/illustrations" title="&#10047; Illustrations" className='lg:ml-4' onClick={handleClick} />
+          <CustomLink href="/13yao/skills" title="&#10047; Skills" className='lg:mx-4' onClick={handleClick} />
+          <CustomLink href="/13yao/projects" title="&#10047; Projects" className='lg:mx-4' onClick={handleClick} />
+          <CustomLink href="/13yao/illustrations" title="&#10047; Illustrations" className='lg:ml-4' onClick={handleClick} />
         </nav>
 
         <nav className="flex items-center justify-center mt-4 lg:mt-0">
@@ -83,10 +83,10 @@ const NavBar = () => {
         </button>
 
         <nav className="flex flex-col items-center mt-20 space-y-6">
-          <CustomLink href="/" title="&#10047; Home" onClick={handleClick} />
-          <CustomLink href="/skills" title="&#10047; Skills" onClick={handleClick} />
-          <CustomLink href="/projects" title="&#10047; Projects" onClick={handleClick} />
-          <CustomLink href="/illustrations" title="&#10047; Illustrations" onClick={handleClick} />
+          <CustomLink href="/13yao" title="&#10047; Home" onClick={handleClick} />
+          <CustomLink href="/13yao/skills" title="&#10047; Skills" onClick={handleClick} />
+          <CustomLink href="/13yao/projects" title="&#10047; Projects" onClick={handleClick} />
+          <CustomLink href="/13yao/illustrations" title="&#10047; Illustrations" onClick={handleClick} />
         </nav>
 
         <nav className="flex items-center justify-center mt-8">
