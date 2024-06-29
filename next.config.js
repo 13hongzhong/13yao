3bb28c8434d13da01e7dc4a4d337037e21e1b1f6
daiqian <cuit980@gmail.com>
2024-06-29 14:42:57 -0500

    Revert "bug fixing if this doesnt fix ill do it tomorrow"
    This reverts commit 177ae27fb23a94a4b02aa4f0b07c8c436f95ffc1.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/13yao', // Set the base path
-  assetPrefix: '/13yao/', // Set the asset prefix
   images: {
-    domains: [], // Add domains for images if needed
+    domains: ['https://13hongzhong.github.io/13yao'], 
   },
-  publicRuntimeConfig: {
-    basePath: '/13yao', // Public runtime config for base path
-  },
-};
+}
 
 module.exports = nextConfig;
